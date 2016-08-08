8b7b95866d4c49e87049909ab77bc1f164bfef20
first <mm8621@163.com>
2016-08-08 19:13:21 +0800

    函数名作为参数传递，执行

--- app.js
+++ app.js
@@ -27,3 +27,19 @@ function f3(){
 }
 
 console.log(f3()());//f3:返回function f3;  f3():返回return的值，此处为匿名函数function;    f3()():匿名函数执行，若里面有return，则将值将作为匿名函数的返回值，如果没有，则最外层的returne将返回undefined
+
+
+function f4(fn){
+    return fn();
+}
+
+function f5(){
+    console.log("调用f5");
+    return "heheda";
+}
+
+console.log(f4(f5));//f5作为参数传给f4
+
+
+
+
