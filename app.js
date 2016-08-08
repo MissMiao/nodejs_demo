9e1488fe026db5fd070bd0524820fc7a1ddafccf
first <mm8621@163.com>
2016-08-08 16:44:28 +0800

    变量 与 函数 声明

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 console.log("the app start");
-i
+var a = 3;
+a = 5;
+a = "hello world";//a="hello world"
+var b = 3+5; //b=8
+var str = a + " hehe";//str = "hello world hehe"
+
+
+
+function f(v){
+   console.log(v);
+}
+console.log("这是第13行");
+f(a);
