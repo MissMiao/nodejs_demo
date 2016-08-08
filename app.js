b063b7e00a939d4ba32c070050a3c95d6c8f0682
first <mm8621@163.com>
2016-08-08 18:23:36 +0800

    匿名函数

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 console.log("the app start");
 
-function a(){
-  return 10*10;
-}
-
-
-function f(){
-    return a;//返回的是function a;不是执行后的结果
-}
-
-
-console.log( (f())() );//f()立即执行得到a,(a)()立即执行a
+(function (){
+console.log("这是一个匿名函数")
+})();
