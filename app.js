91e6cb37071262db6e7df3cc66fc07fac7fca003
first <mm8621@163.com>
2016-08-08 19:23:48 +0800

    创建一个http服务器

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 console.log("the app start");
+var http = require("http");
 
+function CallBackFunction(req,res){
+    res.writeHead(200,{"Content-type":"text/plain"});
+    res.end("hello world\n");
+
+}
+var  server = http.createServer(CallBackFunction);
+
+server.listen(1337);
+
+console.log("Server running at http://127.0.0.1:1337/");
